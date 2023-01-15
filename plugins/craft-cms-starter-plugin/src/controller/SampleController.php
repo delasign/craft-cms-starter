e88c38680ce90975f57d1a88f901893a0f4d4f59
Oscar de la Hera Gomez <oscar@delasign.com>
2023-01-15 12:08:38 -0500

    Initial Controller

---
+++ plugins/craft-cms-starter-plugin/src/controller/SampleController.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * SampleController for Craft CMS 4.x
+ *
+ * A Controller to manage the example of calling a controller action through js.
+ *
+ * @link      https://www.delasign.com
+ * @copyright Copyright (c) 2022 Oscar de la Hera Gomez
+ */
+
+namespace delasign\craftcmsstarterplugin\controllers;
+
+/**
+ * Sample Controller
+ *
+ * This controller carries the actions that help you understand how controller
+ * actions can be invoked through JS.
+ * 
+ * Generally speaking, controllers are the middlemen between the front end of
+ * the CP/website and your module’s services. They contain action methods which
+ * handle individual tasks.
+ *
+ * A common pattern used throughout Craft involves a controller action gathering
+ * post data, saving it on a model, passing the model off to a service, and then
+ * responding to the request appropriately depending on the service method’s response.
+ *
+ * Action methods begin with the prefix “action”, followed by a description of what
+ * the method does (for example, actionSaveIngredient()).
+ *
+ * https://craftcms.com/docs/plugins/controllers
+ *
+ * @author    Oscar de la Hera Gomez
+ * @package   Craft CMS Starter Plugin
+ * @since     1.0.0
+ */
+use Craft;
+use craft\helpers\App;
+use craft\web\Controller;
+use yii\web\HttpException;
+use yii\web\Response;
+// Import the plugin to use the service, via the config
+use delasign\mailservice\MailService;
+
+class SampleController extends Controller
+{
+    // Protected Properties
+    // =========================================================================
+
+     /**
+     * @var    bool|array Allows anonymous access to this controller's actions.
+     *         The actions must be in 'kebab-case'
+     * @access protected
+     */
+    protected int|bool|array $allowAnonymous = true;
+
+    
+    
+    // Public Methods
+    // =========================================================================
+
+    public function beforeAction($action): bool
+    {
+        return parent::beforeAction($action);
+    }
+
+
+    /**
+     * Handle a sample controller action
+     * e.g.: craftcmsstarterplugin/craftcmsstarterplugin/sample-invocation
+     *
+     * @return redirect
+     */
+
+    public function actionSampleInvocation(Int $id): Response
+    { 
+        // All actions must end in a redirect
+        return $this->redirect('craftcmsstarterplugin/?id=' . $id);
+    }
+}
